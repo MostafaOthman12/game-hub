1b2bff9f12459a06d04bfd3537de9168bf7fb563
mostafa othman <mostafayoussef82@gmail.com>
2023-07-14 23:28:42 +0300

    getting optimize images

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -7,7 +7,12 @@ interface Props {
 export const CriticScore = ({ score }: Props) => {
   let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
   return (
-    <Badge paddingX={2} borderRadius={5} fontSize={"14px"} colorScheme={color}>
+    <Badge
+      paddingX={2}
+      borderRadius={5}
+      fontSize={"  14px"}
+      colorScheme={color}
+    >
       {score}
     </Badge>
   );
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../Services/image-url";
 import { CriticScore } from "./CriticScore";
 import { PlatformIconList } from "./PlatformIconList";
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
 export const GameCard = ({ game }: Props) => {
   return (
     <Card overflow="hidden" borderRadius={10}>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
         <HStack justify={"space-between"}>
