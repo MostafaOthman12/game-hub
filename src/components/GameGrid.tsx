ec8f01e2b5d7ec15ac363cbc19e4d4465da0ffb7
mostafa othman <mostafayoussef82@gmail.com>
2023-07-31 01:51:19 +0300

    implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,23 +1,18 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import { GameQurey } from "../App";
 import useGames from "../hooks/useGames";
 import { GameCard } from "./GameCard";
 import { GameCardSkeleton } from "./GameCardSkeleton";
-import React, { useState } from "react";
+import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQurey;
 }
 
 export const GameGrid = ({ gameQuery }: Props) => {
-  const {
-    data,
-    error,
-    isLoading,
-    isFetchingNextPage,
-    hasNextPage,
-    fetchNextPage,
-  } = useGames(gameQuery);
+  const { data, error, isLoading, hasNextPage, fetchNextPage } =
+    useGames(gameQuery);
   const skeltons = [1, 2, 3, 4, 5, 6];
   if (isLoading)
     return (
@@ -32,9 +27,23 @@ export const GameGrid = ({ gameQuery }: Props) => {
       </SimpleGrid>
     );
   return (
-    <Box padding={7}>
+    <InfiniteScroll
+      dataLength={data?.pages.length || 0}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
       {error ? <Text>{error?.message}</Text> : ""}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={4}>
+      <SimpleGrid
+        padding={7}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={4}
+      >
         {data?.pages.map((page, index) => (
           <React.Fragment key={index}>
             {page?.results.map((game) => (
@@ -43,15 +52,6 @@ export const GameGrid = ({ gameQuery }: Props) => {
           </React.Fragment>
         ))}
       </SimpleGrid>
-      {hasNextPage && (
-        <Button
-          marginY={5}
-          onClick={() => fetchNextPage()}
-          colorScheme={"gray"}
-        >
-          {isFetchingNextPage ? "Loading..." : "load more"}
-        </Button>
-      )}
-    </Box>
+    </InfiniteScroll>
   );
 };
