c9ac3a86d9719e4e1842d28761720dde93b5a106
mostafa othman <mostafayoussef82@gmail.com>
2023-08-21 08:30:30 +0300

    fixing fetching bug

--- src/pages/GameDetails.tsx
+++ src/pages/GameDetails.tsx
@@ -1,17 +1,17 @@
-import { Heading, HStack, Text } from "@chakra-ui/react";
+import { Heading, HStack, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
 
 export const GameDetails = () => {
   const { slug } = useParams();
-  const { data, error } = useGame(slug!);
-  if (error || !data) throw error;
-  else {
-    return (
-      <>
-        <Heading> {data.name}</Heading>
-        <Text> {data.description_raw}</Text>
-      </>
-    );
-  }
+  const { data: game, error, isLoading } = useGame(slug!);
+  if (isLoading) return <Spinner />;
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading> {game.name}</Heading>
+      <Text> {game.description_raw}</Text>
+    </>
+  );
 };
